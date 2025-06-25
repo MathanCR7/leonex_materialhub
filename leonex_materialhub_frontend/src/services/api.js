6ad2df223af2b7506fdb37d2534a9909b16751bd
MathanCR7 <mathanleonex123@gmail.com>
2025-06-25 11:02:36 +0530

    Initial commit: Add frontend and backend projects

--- leonex_materialhub_frontend/src/services/api.js
+++ leonex_materialhub_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api"; // Your backend URL
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api"; // Your backend URL
 
 const apiClient = axios.create({
   baseURL: API_URL,
--- leonex_materialhub_frontend/vite.config.js
+++ leonex_materialhub_frontend/vite.config.js
@@ -1,10 +1,11 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // Make sure this line is present
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()], // And this line
+  plugins: [react()],
   server: {
     host: true, // or '0.0.0.0' - for accessing on mobile
+    port: 5002, // Add this line to specify the port
   },
 });
